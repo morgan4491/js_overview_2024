9fef37cfba2d45770a8ebe0eff20c8d87ae6cf16
Life is Good <m.morgan4491@gmail.com>
2024-09-18 11:55:48 -0400

    finished conditioal overview

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
 const firstName = 'Matt';
 let age = 33;
-console.log(age);
 
-// age = 35 // when using 'let' you can change that value later on, unlike 'const'
+// if age is greater than 20 and less than 50, console.log 'you are young!'
 
-// age = age + 5; 
-// age += 5; // shorthand for the above
+// if age is greater than or equal to 50, console.log 'you are getting older'
 
-// age = age - 5;
-// age -= 5; // shorthand for the above
+// if age is less than 20 and greater than 12, console.log 'you are a teenager'
 
-// age = age * 5;
-// age *= 5; // shorthand for the above
-
-// age = age / 5;
-// age /= 5; // shorthand for the above
-
-age = age + 1;
-age++ // shorthand for increasing 'age' by 1 (above)
-
-age = age - 1;
-age-- // shorthand for decreasing 'age' by 1 (above)
-
-console.log(age);
-
-console.log('test');
-console.log(firstName);
+if (age > 20 && age < 50) {
+    console.log('You are young!');
+} else if (age >= 50) {
+    console.log('You are getting older.');
+} else if (age < 20 && age > 12) {
+    console.log('You are a teenager.');
+} else {
+    console.log('Your age is not within our range.')
+}
