f1d90eb8b11d5b3e712a4c960b65b8c527b96973
Life is Good <m.morgan4491@gmail.com>
2024-09-18 11:16:51 -0400

    finished assignment operators

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const firstName = 'Matt';
+let age = 33;
+console.log(age);
+
+// age = 35 // when using 'let' you can change that value later on, unlike 'const'
+
+// age = age + 5; 
+// age += 5; // shorthand for the above
+
+// age = age - 5;
+// age -= 5; // shorthand for the above
+
+// age = age * 5;
+// age *= 5; // shorthand for the above
+
+// age = age / 5;
+// age /= 5; // shorthand for the above
+
+age = age + 1;
+age++ // shorthand for increasing 'age' by 1 (above)
+
+age = age - 1;
+age-- // shorthand for decreasing 'age' by 1 (above)
+
+console.log(age);
+
+console.log('test');
+console.log(firstName);
