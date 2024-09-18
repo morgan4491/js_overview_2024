39b8ccb36c09d84bae1e42f01b4f950208b3381b
Life is Good <m.morgan4491@gmail.com>
2024-09-18 14:49:44 -0400

    discussed functions

--- js/script.js
+++ js/script.js
@@ -1,34 +1,25 @@
-// Arrays are not primitive value types
-// They are reference types
 
-const fruits = ['apple', 'orange', 'kiwi', 'grape', 'banana', 'blueberry', 'strawberry', 'plantain'];
-const data = ['Matt', 'Morgan', 33, '55 Coding Dr', '555-555-5555', ['pickleball', 'fishing']];
+const names = ['Izzy', 'Matt', 'City', 'Kandyce'];
 
-for (let index = 0; index < fruits.length; index++) {
-    console.log(fruits[index]);
+function printNames() {
+    // loop over the names array and print out each name to the console
+    for(let index = 0; index < names.length; index++) {
+        console.log(names[index]);
+    }
 }
 
-const fullName = 'Matt Morgan';
-
-for (let index = 0; index < fullName.length; index++) {
-    console.log(fullName[index]);
-}
+printNames();
 
 
-// arrays are indexed by item
-// strings are indexed by character
+// doSomething();
+// console.log('top');
 
-
-// fruits.length --> gives you the exact # of items in the 'fruits' array
-// *REMEMBER* arrays start at an index# of 0
-
-// can use 'i' instead of 'index' --> see below
-// for (let i = 0; i < fruits.length; i++) {
-//     console.log(fruits[i]);
+// function doSomething() {
+//     console.log('do something!');
+//     console.log('another something');
+//     console.log('one more thing');
 // }
 
-// for (let count = 0; count < 11; count++) {
-//     console.log(count);
-// }
-
-// *Loops have to finish before continuing onto the next line of code
+// doSomething();
+// doSomething();
+// doSomething();
