a44db145e187dbc466873e5eeb785850186ff9ce
Life is Good <m.morgan4491@gmail.com>
2024-09-19 15:06:20 -0400

    started object basics

--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
-const notes = ['JS is cool', 'create a constant with const', 'use the src attribute on the script tag to load a js file'];
 
-console.log(notes);
+// define an object using {}
+const user = {
+    name: 'Matt',  // 'name' here is a property
+    age: 33,
+    phone: '777-777-7777',
+    address: '555 coding dr',
+    info: {
+        location: 'Atlanta area',
+        hobbies: ['pickleball', 'fishing']
+    },
+    printAge: function() {                      // printAge is a 'method'
+        console.log(this.age);
+    },
+    haveBirthday: function() {                  // haveBirthday is also a 'method'
+        this.age++;
+        console.log('Happy Birthday');
+    },
+    addHobby: function() {
+        // Need to reference an argument that is passed to addHobby (string of a hobby)
+        user.info.hobbies.push('surfing');
+        // Push the argument (hobby) to the info.hobbies array
+    },
+    printHobbies: function() {
+        // console.log the hobbies array
+        console.log(user.info.hobbies[2]);
+        // BONUS - loop over the hobbies array and console.log each hobby
+        for (let index = 0; index < user.info.hobbies.length; index++) {
+            console.log(user.info.hobbies[index]);
+        }
+    }
+};
+
+// user.haveBirthday();
+
+// user.printAge();
+
+user.addHobby();
+
+user.printHobbies();
+
+// for (let index = 0; index < user.info.hobbies.length; index++) {
+//     console.log(user.info.hobbies[index]);
+// }
 
 
-notes.push('Copilot is a great help!'); // 'push' adds a new item to te end of the array
+// if (!user.height) {  // notice the use of the ! (not) in front of user.height - this conditional statement will only execute is the height has not been provided
+//     console.log('You have not provided your height')
+// }
 
-console.log(notes);
+// console.log(user.age);
 
-notes.unshift('CSS is crazy'); // 'unshift' adds an item to the beginning of the array
-
-console.log(notes);
-
-notes.pop(); //'pop' removes the last item in the array
-
-console.log(notes);
-
-notes.shift(); // 'shift' removes the first item in the array
-
-console.log(notes);
-
-
-
-const notes2 = notes;
-
-notes2.splice(1, 1); // 'splice' removes an item at a certain index - pass in the index of where you want to start deleting items and then the amount of items you want to delete
-
-console.log(notes2);
+// console.log(user.info.hobbies[1]);
